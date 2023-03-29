e9aadd8c96133f1c216085b5fc3f0072e9c0a268
Luiz Sócrate <luiz@socrate.com.br>
2023-03-29 11:56:00 +0200

    Improve

--- src/image-json-generator.ts
+++ src/image-json-generator.ts
@@ -7,12 +7,154 @@ if (!pack || !packCode) {
 }
 
 const [traitMap, allCards] = await Promise.all([fetchTraits(), fetchCards()]);
-const packCards = allCards.filter((c) =>
-  c.versions.some((v) => v.pack_id === pack)
-);
-if (packCards.length < 1) {
-  console.error("the chosen pack has no cards");
-  process.exit(1);
+
+main(pack, allCards);
+
+function main(pack: string, allCards: Card[]) {
+  const packCards = allCards.filter((c) =>
+    c.versions.some((v) => v.pack_id === pack)
+  );
+  if (packCards.length < 1) {
+    console.error("the chosen pack has no cards");
+    process.exit(1);
+  }
+
+  const artJson = packCards.flatMap(toImageJson);
+  const sortedJson = artJson.sort(
+    (a, b) => parseInt(a.card_id, 10) - parseInt(b.card_id, 10)
+  );
+
+  console.log(JSON.stringify(sortedJson));
+}
+
+function toImageJson(
+  c: Card
+): Array<
+  | ReturnType<typeof toProvince>
+  | ReturnType<typeof toHolding>
+  | ReturnType<typeof toAttachment>
+  | ReturnType<typeof toCharacter>
+  | ReturnType<typeof toEvent>
+  | ReturnType<typeof toStronghold>
+> {
+  switch (c.type) {
+    case "province":
+      return [toProvince(c)];
+    case "holding":
+      return [toHolding(c)];
+    case "attachment":
+      return [toAttachment(c)];
+    case "character":
+      return [toCharacter(c)];
+    case "event":
+      return [toEvent(c)];
+    case "stronghold":
+      return [toStronghold(c)];
+    default:
+      return [];
+  }
+}
+
+function toStronghold(c: Card) {
+  return {
+    artist: artist(c),
+    artwork: artwork(c),
+    card_frame: `frames/${c.faction}_stronghold.png`,
+    card_id: cardId(c),
+    stronghold_fate: c.fate ?? 7,
+    stronghold_flavour: flavor(c),
+    stronghold_honour: c.honor ?? 99,
+    stronghold_influence: c.influence_pool ?? 10,
+    stronghold_modifier: c.strength_bonus?.[0] ?? "+",
+    stronghold_modifier_value: c.strength_bonus
+      ? parseInt(c.strength_bonus.slice(1), 10)
+      : 0,
+    stronghold_text: text(c),
+    stronghold_title: title(c),
+    stronghold_traits: traits(c),
+  };
+}
+
+function toProvince(c: Card) {
+  return {
+    artist: artist(c),
+    artwork: artwork(c),
+    card_frame: `frames/${c.faction}_province.png`,
+    card_id: cardId(c),
+    ...provinceElements(c),
+    province_flavour: flavor(c),
+    province_strength: c.strength,
+    province_text: text(c),
+    province_title: title(c),
+    province_traits: traits(c),
+  };
+}
+
+function toCharacter(c: Card) {
+  return {
+    artist: artist(c),
+    artwork: artwork(c),
+    card_frame: `frames/${c.faction}_character.png`,
+    card_id: cardId(c),
+    character_flavour: flavor(c),
+    character_glory: c.glory ?? 0,
+    character_military: c.military == undefined ? "-" : c.military,
+    character_political: c.political == undefined ? "-" : c.political,
+    character_text: text(c),
+    character_title: title(c),
+    character_traits: traits(c),
+    deck_type: deckType(c),
+    fate_cost: fateCost(c),
+    influence: influence(c),
+  };
+}
+
+function toEvent(c: Card) {
+  return {
+    artist: artist(c),
+    artwork: artwork(c),
+    card_frame: `frames/${c.faction}_event.png`,
+    card_id: cardId(c),
+    deck_type: deckType(c),
+    event_flavour: flavor(c),
+    event_text: text(c),
+    event_title: title(c),
+    event_traits: traits(c),
+    fate_cost: fateCost(c),
+    influence: influence(c),
+  };
+}
+
+function toAttachment(c: Card) {
+  return {
+    artist: artist(c),
+    artwork: artwork(c),
+    attachment_flavour: flavor(c),
+    ...skillModifier(c),
+    attachment_text: text(c),
+    attachment_title: title(c),
+    attachment_traits: traits(c),
+    card_frame: `frames/${c.faction}_attachment.png`,
+    card_id: cardId(c),
+    deck_type: deckType(c),
+    fate_cost: fateCost(c),
+    influence: influence(c),
+  };
+}
+
+function toHolding(c: Card) {
+  return {
+    artist: artist(c),
+    artwork: artwork(c),
+    card_frame: `frames/${c.faction}_holding.png`,
+    card_id: cardId(c),
+    deck_type: deckType(c),
+    holding_flavour: flavor(c),
+    ...holdingModifier(c),
+    holding_text: text(c),
+    holding_title: title(c),
+    holding_traits: traits(c),
+  };
 }
 
 function title(c: Card): string {
@@ -35,9 +177,11 @@ function text(c: Card): string {
   return c.text.length < 1
     ? ""
     : c.text
-        .replaceAll("<br/>", "\n")
-        .replace(/[+-][X\d]/g, (match) =>
-          match[0] === "+" ? `+${match[1]}` : `−${match[1]}`
+        .replaceAll("<br/>", "\n") // line breaks
+        .replaceAll(" - ", " – ") // hyphen to en-dash on cost - effect separation
+        .replace(
+          /[+-][X\d]/g,
+          (match) => (match[0] === "+" ? `+${match[1]}` : `−${match[1]}`) // proper math symbols
         );
 }
 
@@ -81,7 +225,7 @@ function provinceElements(c: Card) {
 }
 
 function fateCost(c: Card) {
-  return c.cost ?? "-";
+  return typeof c.cost === "string" && c.cost.length > 0 ? c.cost : "-";
 }
 
 function artist(c: Card): string {
@@ -123,117 +267,3 @@ function skillModifier(c: Card) {
   }
   return mods;
 }
-
-const toStronghold = (c: Card) => ({
-  artist: artist(c),
-  artwork: artwork(c),
-  card_frame: `frames/${c.faction}_stronghold.png`,
-  card_id: cardId(c),
-  stronghold_fate: c.fate ?? 7,
-  stronghold_flavour: flavor(c),
-  stronghold_honour: c.honor ?? 99,
-  stronghold_influence: c.influence_pool ?? 10,
-  stronghold_modifier: c.strength_bonus?.[0] ?? "+",
-  stronghold_modifier_value: c.strength_bonus
-    ? parseInt(c.strength_bonus.slice(1), 10)
-    : 0,
-  stronghold_text: text(c),
-  stronghold_title: title(c),
-  stronghold_traits: traits(c),
-});
-
-const toProvince = (c: Card) => ({
-  artist: artist(c),
-  artwork: artwork(c),
-  card_frame: `frames/${c.faction}_province.png`,
-  card_id: cardId(c),
-  ...provinceElements(c),
-  province_flavour: flavor(c),
-  province_strength: c.strength,
-  province_text: text(c),
-  province_title: title(c),
-  province_traits: traits(c),
-});
-
-const toCharacter = (c: Card) => ({
-  artist: artist(c),
-  artwork: artwork(c),
-  card_frame: `frames/${c.faction}_character.png`,
-  card_id: cardId(c),
-  character_flavour: flavor(c),
-  character_glory: c.glory ?? 0,
-  character_military: c.military == undefined ? "-" : c.military,
-  character_political: c.political == undefined ? "-" : c.political,
-  character_text: text(c),
-  character_title: title(c),
-  character_traits: traits(c),
-  deck_type: deckType(c),
-  fate_cost: fateCost(c),
-  influence: influence(c),
-});
-
-const toEvent = (c: Card) => ({
-  artist: artist(c),
-  artwork: artwork(c),
-  card_frame: `frames/${c.faction}_event.png`,
-  card_id: cardId(c),
-  deck_type: deckType(c),
-  event_flavour: flavor(c),
-  event_text: text(c),
-  event_title: title(c),
-  event_traits: traits(c),
-  fate_cost: fateCost(c),
-  influence: influence(c),
-});
-
-const toAttachment = (c: Card) => ({
-  artist: artist(c),
-  artwork: artwork(c),
-  attachment_flavour: flavor(c),
-  ...skillModifier(c),
-  attachment_text: text(c),
-  attachment_title: title(c),
-  attachment_traits: traits(c),
-  card_frame: `frames/${c.faction}_attachment.png`,
-  card_id: cardId(c),
-  deck_type: deckType(c),
-  fate_cost: fateCost(c),
-  influence: influence(c),
-});
-
-const toHolding = (c: Card) => ({
-  artist: artist(c),
-  artwork: artwork(c),
-  card_frame: `frames/${c.faction}_holding.png`,
-  card_id: cardId(c),
-  deck_type: deckType(c),
-  holding_flavour: flavor(c),
-  ...holdingModifier(c),
-  holding_text: text(c),
-  holding_title: title(c),
-  holding_traits: traits(c),
-});
-
-const toImageJson = (c: Card): any[] => {
-  switch (c.type) {
-    case "province":
-      return [toProvince(c)];
-    case "holding":
-      return [toHolding(c)];
-    case "attachment":
-      return [toAttachment(c)];
-    case "character":
-      return [toCharacter(c)];
-    case "event":
-      return [toEvent(c)];
-    case "stronghold":
-      return [toStronghold(c)];
-    default:
-      return [];
-  }
-};
-
-const artJson = packCards.flatMap(toImageJson);
-const sortedJson = artJson.sort((a, b) => a.card_id.localeCompare(b.card_id));
-
-console.log(JSON.stringify(sortedJson));
